77c80cf9633d3fbe019603ed676a3fb18ed9b608
Gopal Patel <gopalp4716@gmail.com>
2025-01-30 23:09:42 -0500

    Initial Commit

---
+++ Fit 2Tests/Fit_2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Fit_2Tests.swift
+//  Fit 2Tests
+//
+//  Created by Gopal Patel on 1/30/25.
+//
+
+import Testing
+@testable import Fit_2
+
+struct Fit_2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Fit 2UITests/Fit_2UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Fit_2UITestsLaunchTests.swift
+//  Fit 2UITests
+//
+//  Created by Gopal Patel on 1/30/25.
+//
+
+import XCTest
+
+final class Fit_2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
